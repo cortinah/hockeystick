d20b0d1c9c2bd262d6098def80c0fe2038703950
Hernando <hch@alum.mit,edu>
2020-08-10 18:27:58 -0400

    Add Vostok paleo functions

--- R/globalvars.R
+++ R/globalvars.R
@@ -1,2 +1,3 @@
 # Setting global variables to avoid R CMD check notices
-utils::globalVariables(c('interpolated', 'trend', 'Year', 'J-D', 'gmsl', 'method', 'month'))
+utils::globalVariables(c('interpolated', 'trend', 'Year', 'J-D', 'gmsl', 'method', 'month',
+                         'age_ice', 'co2', 'temp', 'value'))
