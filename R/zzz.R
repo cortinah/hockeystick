9c67b6002107c97f3945a97bba89b7fb930e3846
Hernando <hch@alum.mit.edu>
2021-07-04 19:09:05 -0400

    don't forget utils::

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   options(hs_write_cache=getOption("hs_write_cache", FALSE))
 
-  packageStartupMessage(paste0("hockeystick ", packageVersion("hockeystick"),": Use hockeystick_cache_details() to view cached climate data.\n", "hockeystick ", packageVersion("hockeystick"), ": Use hockeystick_update_all() to update and cache all climate data."))
+  packageStartupMessage(paste0("hockeystick ", utils::packageVersion("hockeystick"),": Use hockeystick_cache_details() to view cached climate data.\n", "hockeystick ", utils::packageVersion("hockeystick"), ": Use hockeystick_update_all() to update and cache all climate data."))
 }
