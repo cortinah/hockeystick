dd97846e6b49d578956fde4fa66ff532f68928cc
Hernando Cortina <hch@alum.mit.edu>
2021-07-04 19:26:49 -0400

    Add version info on startup (#11)
    * Add version info on startup
    
    * don't forget utils::

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   options(hs_write_cache=getOption("hs_write_cache", FALSE))
-  packageStartupMessage("hockeystick: Use hockeystick_cache_details() to view cached climate data.\nhockeystick: Use hockeystick_update_all() to update and cache all climate data.")
- }
+
+  packageStartupMessage(paste0("hockeystick ", utils::packageVersion("hockeystick"),": Use hockeystick_cache_details() to view cached climate data.\n", "hockeystick ", utils::packageVersion("hockeystick"), ": Use hockeystick_update_all() to update and cache all climate data."))
+}
