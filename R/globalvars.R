38d4af5500dfbedb16697209b866349325d73240
Hernando Cortina <hch@alum.mit.edu>
2022-09-14 05:53:30 -0400

    Amend global vars

--- R/globalvars.R
+++ R/globalvars.R
@@ -1,3 +1,3 @@
 # Setting global variables to avoid R CMD check notices
 utils::globalVariables(c('average', 'trend', 'year','Year', 'J-D', 'gmsl', 'method', 'month',
-                         'age_ice', 'co2', 'temp', 'value', 'size', 'measure','RevisedMajorHurricanes', 'RevisedACE', 'iso_code'))
+                         'age_ice', 'co2', 'temp', 'value', 'size', 'measure','RevisedMajorHurricanes', 'RevisedACE', 'country'))
