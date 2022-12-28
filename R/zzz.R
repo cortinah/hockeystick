5fac1b081443858030f7c80e333fb0e01d292cef
Hernando Cortina <hch@alum.mit.edu>
2022-12-27 20:46:23 -0500

    Add logo

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,22 @@
 .onAttach <- function(libname, pkgname) {
   options(hs_write_cache=getOption("hs_write_cache", FALSE))
+  hs_logo <- "                                                                 _>>>>_-
+                                                               -****<*<__-
+                                                              >)****<*<___
+                                                             <])****<*<__
+                                                           -/)])****<*<_
+                                                          >]])])****<<-
+                                                        _/]]])])****_
+                                                      _)]]]]])])**_
+                                                    _*/]]]]]])])_
+                                                  _/]]/]]]]]]*_
+                                               _>*/]]]/]]]]*_
+                                         --_>**)**/]]]/]/>-
+                       - ---_______>>><**)><*)*)**/]](>_
+-_______-_--__>__>__>>_>_>><****>>_>>_>>>>_______----"
 
-  packageStartupMessage(paste0("hockeystick ", utils::packageVersion("hockeystick"),": Use hockeystick_cache_details() to view cached climate data.\n", "hockeystick ", utils::packageVersion("hockeystick"), ": Use hockeystick_update_all() to update and cache all climate data."))
+  hs_cols <- c("#0066FF","#FFFF00","#FF6000","#FF0000","#FF0000")
+  if (!isTRUE(getOption("knitr.in.progress"))) multicolor::multi_color(hs_logo, colors = hs_cols)
+  packageStartupMessage(paste0("hockeystick ", utils::packageVersion("hockeystick"),": Use hockeystick_cache_details() to view cached climate data.\n", "hockeystick ",
+                               utils::packageVersion("hockeystick"), ": Use hockeystick_update_all() to update and cache all climate data."))
 }
