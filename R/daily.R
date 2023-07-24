ac9fc6c99b3076eb758ab5f7d0d39b4a7d8ea3c4
Hernando Cortina <hch@alum.mit.edu>
2023-07-24 12:56:26 -0400

    improvements

--- R/daily.R
+++ R/daily.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(jsonlite)
-#library(splitstackshape)
 library(RColorBrewer)
 
 # pull daily temperature from https://climatereanalyzer.org/clim/t2_daily/
 temp_json <- fromJSON('https://climatereanalyzer.org/clim/t2_daily/json/cfsr_world_t2_day.json')
+
+#temp_json <- fromJSON('https://climatereanalyzer.org/clim/t2_daily/json/cfsr_antarctic_t2_day.json')
 
 leap_years <- seq.int(1980, 2028, 4)
 
@@ -38,27 +39,25 @@ dates <- data.frame(day_of_year=1:365,
 
 daily_temperature <- left_join(daily_temperature, dates)
 
+latest <- paste(pull(tail(daily_temperature,1)[1]),substr(pull(tail(daily_temperature,1)[6]),6,7), substr(pull(tail(daily_temperature,1)[6]),9,10),sep = '-')
 # draw plot
-ggplot(daily_temperature) +
-  geom_line(aes(date, temp, group = year), color = 'grey', alpha=0.7) +
+  ggplot(daily_temperature) +
+  geom_line(aes(date, temp, group = year), color = 'grey', alpha = 0.7) +
   scale_fill_gradientn(name='Anomaly (C\U00B0)', colors = brewer.pal(9, 'YlOrRd')) +
-  geom_line(aes(date, mean_temp), color = 'black', linetype = 'dashed', linewidth=1.1) +
-  geom_line(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(), 1, 4))), aes(date, temp), colour = "firebrick", linewidth=2) +
-  scale_y_continuous(limits=c(11,18), breaks=11:18) +
+  geom_line(aes(date, mean_temp), color = 'black', linetype = 'dashed', linewidth = 1.1) +
+  geom_line(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(), 1, 4))),
+            aes(date, temp), colour = "firebrick", linewidth = 1.5) +
+  scale_y_continuous(n.breaks = 9) +
   theme_bw(base_size = 12) +
-  scale_x_date(name=element_blank(), breaks=c(as.Date('1975-01-01'), as.Date('1975-04-01'),
-                                            as.Date('1975-07-01'),as.Date('1975-10-01'),
-                                            as.Date('1975-12-31')),
-               date_labels = '%b-%d',
-               date_minor_breaks = '1 month') +
-
-  geom_rect(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(),1,4))),
-            aes(xmin = date-1,
-                xmax = date,
-                ymin = mean_temp,
-                ymax = mean_temp + temp_anom,
-                fill = temp_anom)) +
-  theme(legend.position="top") + theme(legend.key.size = unit(0.5, 'cm'),
-                                       legend.margin = margin(5, 0, 0, 0)) +
-  labs(title='Global Daily Average 2-Meter Air Temperature', subtitle='Daily since 1979, 1979-2000 mean, and current anomaly',
-       y='Temperature (C\U00B0)', caption='Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org')
+  scale_x_date(name=element_blank(), breaks = c(as.Date('1975-01-01'), as.Date('1975-04-01'),
+               as.Date('1975-07-01'), as.Date('1975-10-01'), as.Date('1975-12-31')),
+               date_labels = '%b-%d', date_minor_breaks = '1 month') +
+  labs(title = 'Daily Global Average Air Temperature', subtitle = '2-meter temperature since 1979, 1979-2000 mean, and current anomaly',
+       y = 'Temperature (C\U00B0)', caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest)) + geom_rect(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(),1,4))),
+          aes(xmin = date-1,
+              xmax = date,
+              ymin = mean_temp,
+              ymax = mean_temp + temp_anom,
+              fill = temp_anom)) +
+theme(legend.position="top") + theme(legend.key.size = unit(0.5, 'cm'),
+                                     legend.margin = margin(5, 0, 0, 0))
