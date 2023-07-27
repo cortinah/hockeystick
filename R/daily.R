97b77f29b13ab8cf571b29008c0b6b050efa28d0
Hernando Cortina <hch@alum.mit.edu>
2023-07-26 23:07:07 -0400

    legends

--- R/daily.R
+++ R/daily.R
@@ -40,24 +40,33 @@ dates <- data.frame(day_of_year=1:365,
 daily_temperature <- left_join(daily_temperature, dates)
 
 latest <- paste(pull(tail(daily_temperature,1)[1]),substr(pull(tail(daily_temperature,1)[6]),6,7), substr(pull(tail(daily_temperature,1)[6]),9,10),sep = '-')
+current_year <- as.numeric(substr(Sys.Date(), 1, 4))
+
 # draw plot
+
+
   ggplot(daily_temperature) +
-  geom_line(aes(date, temp, group = year), color = 'grey', alpha = 0.7) +
-  scale_fill_gradientn(name='Anomaly (C\U00B0)', colors = brewer.pal(9, 'YlOrRd')) +
-  geom_line(aes(date, mean_temp), color = 'black', linetype = 'dashed', linewidth = 1.1) +
-  geom_line(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(), 1, 4))),
-            aes(date, temp), colour = "firebrick", linewidth = 1.5) +
-  scale_y_continuous(n.breaks = 9) +
-  theme_bw(base_size = 12) +
-  scale_x_date(name=element_blank(), breaks = c(as.Date('1975-01-01'), as.Date('1975-04-01'),
-               as.Date('1975-07-01'), as.Date('1975-10-01'), as.Date('1975-12-31')),
-               date_labels = '%b-%d', date_minor_breaks = '1 month') +
-  labs(title = 'Daily Global Average Air Temperature', subtitle = '2-meter temperature since 1979, 1979-2000 mean, and current anomaly',
-       y = 'Temperature (C\U00B0)', caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest)) + geom_rect(data = filter(daily_temperature, year == as.numeric(substr(Sys.Date(),1,4))),
-          aes(xmin = date-1,
-              xmax = date,
-              ymin = mean_temp,
-              ymax = mean_temp + temp_anom,
-              fill = temp_anom)) +
-theme(legend.position="top") + theme(legend.key.size = unit(0.5, 'cm'),
-                                     legend.margin = margin(5, 0, 0, 0))
+    geom_line(aes(date, temp, group = year), alpha = 0.7, color='grey') +
+    scale_fill_gradientn(name='Anomaly (C\U00B0)', colors = brewer.pal(9, 'YlOrRd')) +
+    geom_line(aes(date, mean_temp, color = 'M'), linetype = 'dashed', linewidth = 1.1) +
+
+    scale_y_continuous(n.breaks = 9) +
+    theme_bw(base_size = 12) +
+    scale_x_date(name=element_blank(), breaks = c(as.Date('1975-01-01'), as.Date('1975-04-01'),
+                                                  as.Date('1975-07-01'), as.Date('1975-10-01'), as.Date('1975-12-31')),
+                 date_labels = '%b-%d', date_minor_breaks = '1 month') +
+    labs(title = 'Daily Global Average Air Temperature', subtitle = '2-meter temperature since 1979, mean, and current anomaly',
+         y = 'Temperature (C\U00B0)', caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest),
+         color=NULL) +
+    scale_color_manual(values=c('firebrick', 'black', 'grey'), labels = c(current_year, '1979-2000 Mean'), breaks=c('L', 'M')) +
+    geom_rect(data = filter(daily_temperature, year == current_year),
+              aes(xmin = date - 1,
+                  xmax = date,
+                  ymin = mean_temp,
+                  ymax = mean_temp + temp_anom,
+                  fill = temp_anom)) +
+    geom_line(data = filter(daily_temperature, year == current_year),
+              aes(date, temp, color='L'), linewidth = 1.3) +
+    theme(legend.position="top") + theme(legend.key.size = unit(0.5, 'cm'),
+                                         legend.margin = margin(5, 0, 0, 0))
+
