cd33706a44ff062e07270bd2802a003b1e32387f
Hernando Cortina <hch@alum.mit.edu>
2023-07-27 00:01:21 -0400

    adj mean years

--- R/daily.R
+++ R/daily.R
@@ -3,7 +3,8 @@ library(jsonlite)
 library(RColorBrewer)
 
 
-get_dailytemp <- function(use_cache = TRUE, write_cache = getOption("hs_write_cache")) {
+get_dailytemp <- function(use_cache = TRUE, write_cache = getOption("hs_write_cache"),
+                          mean_start = 1979, mean_end = 2020) {
 
   hs_path <- tools::R_user_dir("hockeystick","cache")
 
@@ -40,7 +41,7 @@ daily_temperature <- as.data.frame(temp_json) |>
 daily_temperature <- daily_temperature |> group_by(year) |>
   mutate(day_of_year = row_number()) |> ungroup() |>
   group_by(day_of_year) |>
-  mutate(mean_temp = mean(temp[year >= 1979 & year <= 2000]),
+  mutate(mean_temp = mean(temp[year >= mean_start & year <= mean_end]),
          temp_anom = temp - mean_temp) |>
   ungroup()
 
@@ -57,12 +58,11 @@ invisible(daily_temperature)
 
 # draw plot
 
-plot_dailytemp <- function(dataset = get_dailytemp(), print=TRUE, anomaly = TRUE) {
+plot_dailytemp <- function(dataset = get_dailytemp(), print=TRUE, anomaly = TRUE, current_year = as.numeric(substr(Sys.Date(), 1, 4))) {
 
   if (is.null(dataset)) return(invisible(NULL))
 
 latest <- paste(pull(tail(dataset,1)[1]), substr(pull(tail(dataset,1)[6]),6,7), substr(pull(tail(dataset,1)[6]),9,10), sep = '-')
-current_year <- as.numeric(substr(Sys.Date(), 1, 4))
 
 plot <- ggplot(dataset) +
     geom_line(aes(dummy_date, temp, group = year), alpha = 0.7, color='grey') +
@@ -73,7 +73,7 @@ plot <- ggplot(dataset) +
     scale_x_date(name=element_blank(), breaks = c(as.Date('1975-01-01'), as.Date('1975-04-01'),
                                                   as.Date('1975-07-01'), as.Date('1975-10-01'), as.Date('1975-12-31')),
                  date_labels = '%b-%d', date_minor_breaks = '1 month') +
-    labs(title = 'Daily Global Average Air Temperature', subtitle = '2-meter temperature since 1979, mean, and current anomaly',
+    labs(title = 'Daily Global Average Air Temperature', subtitle = '2-meter temperature since 1979 and mean',
          y = 'Temperature (C\U00B0)',
          caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest),
          color = NULL) +
@@ -91,7 +91,8 @@ if (anomaly) plot <- plot +
               ymax = mean_temp + temp_anom,
               fill = temp_anom)) +
           geom_line(data = filter(dataset, year == current_year),
-            aes(dummy_date, temp, color='L'), linewidth = 1.3)
+            aes(dummy_date, temp, color='L'), linewidth = 1.3) +
+  labs(subtitle = '2-meter temperature since 1979, mean, and current anomaly')
 
   if (print) suppressMessages( print(plot) )
   invisible(plot)
