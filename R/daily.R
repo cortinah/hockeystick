8aabe1fe9fae93d369c51eef3cf8591a94d8086c
Hernando Cortina <hch@alum.mit.edu>
2023-07-26 23:43:41 -0400

    functionalize

--- R/daily.R
+++ R/daily.R
@@ -2,10 +2,22 @@ library(tidyverse)
 library(jsonlite)
 library(RColorBrewer)
 
-# pull daily temperature from https://climatereanalyzer.org/clim/t2_daily/
-temp_json <- fromJSON('https://climatereanalyzer.org/clim/t2_daily/json/cfsr_world_t2_day.json')
 
-#temp_json <- fromJSON('https://climatereanalyzer.org/clim/t2_daily/json/cfsr_antarctic_t2_day.json')
+get_dailytemp <- function(use_cache = TRUE, write_cache = getOption("hs_write_cache")) {
+
+  hs_path <- tools::R_user_dir("hockeystick","cache")
+
+  if (use_cache) {
+    if (file.exists(file.path(hs_path,'dailytemp.rds'))) return(invisible(readRDS((file.path(hs_path,'dailytemp.rds')))))
+  }
+
+  file_url <- 'https://climatereanalyzer.org/clim/t2_daily/json/cfsr_world_t2_day.json'
+  connected <- .isConnected(file_url)
+  if (!connected) {message("Retrieving remote data requires internet connectivity."); return(invisible(NULL))}
+
+  dl <- tempfile()
+  download.file(file_url, dl)
+  temp_json <- fromJSON(dl)
 
 leap_years <- seq.int(1980, 2028, 4)
 
@@ -32,23 +44,30 @@ daily_temperature <- daily_temperature |> group_by(year) |>
          temp_anom = temp - mean_temp) |>
   ungroup()
 
-
 dates <- data.frame(day_of_year=1:365,
-                    date=seq(as.Date('1975-01-01'), as.Date('1975-12-31'), by = '1 days'))
+                    dummy_date=seq(as.Date('1975-01-01'), as.Date('1975-12-31'), by = '1 days'))
 
+daily_temperature <- left_join(daily_temperature, dates, by='day_of_year')
 
-daily_temperature <- left_join(daily_temperature, dates)
+if (write_cache) saveRDS(daily_temperature, file.path(hs_path, 'dailytemp.rds'))
 
-latest <- paste(pull(tail(daily_temperature,1)[1]),substr(pull(tail(daily_temperature,1)[6]),6,7), substr(pull(tail(daily_temperature,1)[6]),9,10),sep = '-')
-current_year <- as.numeric(substr(Sys.Date(), 1, 4))
+invisible(daily_temperature)
+}
+
 
 # draw plot
 
+plot_dailytemp <- function(dataset = get_dailytemp(), print=TRUE) {
 
-  ggplot(daily_temperature) +
-    geom_line(aes(date, temp, group = year), alpha = 0.7, color='grey') +
+  if (is.null(dataset)) return(invisible(NULL))
+
+latest <- paste(pull(tail(dataset,1)[1]),substr(pull(tail(dataset,1)[6]),6,7), substr(pull(tail(dataset,1)[6]),9,10),sep = '-')
+current_year <- as.numeric(substr(Sys.Date(), 1, 4))
+
+plot <- ggplot(dataset) +
+    geom_line(aes(dummy_date, temp, group = year), alpha = 0.7, color='grey') +
     scale_fill_gradientn(name='Anomaly (C\U00B0)', colors = brewer.pal(9, 'YlOrRd')) +
-    geom_line(aes(date, mean_temp, color = 'M'), linetype = 'dashed', linewidth = 1.1) +
+    geom_line(aes(dummy_date, mean_temp, color = 'M'), linetype = 'dashed', linewidth = 1.1) +
     scale_y_continuous(n.breaks = 9) +
     theme_bw(base_size = 12) +
     scale_x_date(name=element_blank(), breaks = c(as.Date('1975-01-01'), as.Date('1975-04-01'),
@@ -59,14 +78,17 @@ current_year <- as.numeric(substr(Sys.Date(), 1, 4))
          caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest),
          color = NULL) +
     scale_color_manual(values=c('firebrick', 'black', 'grey'), labels = c(current_year, '1979-2000 Mean'), breaks=c('L', 'M')) +
-    geom_rect(data = filter(daily_temperature, year == current_year),
-              aes(xmin = date - 1,
-                  xmax = date,
+    geom_rect(data = filter(dataset, year == current_year),
+              aes(xmin = dummy_date - 1,
+                  xmax = dummy_date,
                   ymin = mean_temp,
                   ymax = mean_temp + temp_anom,
                   fill = temp_anom)) +
-    geom_line(data = filter(daily_temperature, year == current_year),
-              aes(date, temp, color='L'), linewidth = 1.3) +
+    geom_line(data = filter(dataset, year == current_year),
+              aes(dummy_date, temp, color='L'), linewidth = 1.3) +
     theme(legend.position="top") + theme(legend.key.size = unit(0.5, 'cm'),
                                          legend.margin = margin(5, 0, 0, 0))
 
+  if (print) suppressMessages( print(plot) )
+  invisible(plot)
+}
