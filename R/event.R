bf541b2d5bdde6dd372e1934a6681fe07b5c53c3
Hernando Cortina <hch@alum.mit.edu>
2023-10-15 10:50:35 -0400

    November update

--- R/event.R
+++ R/event.R
@@ -4,69 +4,80 @@ old <- options(pillar.sigfig = 4)
 options(digits=4)
 ### Global heat
 
-t <- get_temp(use_cache = F, write_cache = T)
+t <- get_temp(use_cache = T, write_cache = F)
 
-t |> rowwise() |> mutate(ytd=mean(c(Jan,Feb,Mar,Apr,May,Jun,Jul,Aug))) |> ungroup() -> t
+t |> rowwise() |> mutate(ytd=mean(c(Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct))) |> ungroup() -> t
 
-#Jan-Aug actual: 1.06
+#Jan-Oct actual: 1.121
 
 t |> mutate(diff=`J-D`-ytd) -> t
 
 
-tail(head(t,-1), 5) |> summarize(diff=mean(diff)) |> as.numeric() -> temp_diff
-slice_tail(t,n=1) |> select(ytd) |> as.numeric() + temp_diff
+tail(head(t,-1), 7) |> summarize(diff=mean(diff)) |> as.numeric() -> temp_diff
+slice_tail(t, n=1) |> select(ytd) |> as.numeric() + temp_diff
 
-# Implies annual avg: 1.069 20-yr
-# Implies annual avg: 1.067 10-yr
-# Implies annual avg: 1.051 7-yr
-# Implies annual avg: 1.064 5-yr
+
+# Implies annual avg: 1.102 20-yr
+# Implies annual avg: 1.10 10-yr
+# Implies annual avg: 1.09 7-yr
+# Implies annual avg: 1.098 5-yr
 
 
 ## daily temps
 
 d <- get_dailytemp(use_cache = F, write_cache = T)
 
-d |> filter(year!=2023) |> group_by(year) |> filter(dummy_date>=as.Date('1975-09-01')) |>
-  filter(dummy_date<as.Date('1975-10-01')) |>
+d |> filter(year!=2023) |> group_by(year) |> filter(dummy_date>=as.Date('1975-11-01')) |>
+  filter(dummy_date<as.Date('1975-12-01')) |>
   summarize(dailyanom=mean(temp_anom)) -> dailyanom
 
 colnames(dailyanom)[1]='Year'; dailyanom$Year <- as.Date(paste0(as.character(dailyanom$Year),'-12-31'))
 
 t <- left_join(t, dailyanom)
-t |> mutate(dailydiff = Sep - dailyanom) -> t
+
+# update month
+t |> mutate(dailydiff = Nov - dailyanom) -> t
 
 
 tail(head(t,-1), 7) |> summarize(anom=mean(dailydiff)) |> as.numeric() -> daily_diff
 
-d |> filter(year==2023) |> filter(dummy_date>=as.Date('1975-09-01')) |>
-  filter(dummy_date<as.Date('1975-10-01')) |>
+d |> filter(year==2023) |> filter(dummy_date>=as.Date('1975-11-01')) |>
+  filter(dummy_date<as.Date('1975-12-01')) |>
   summarize(anom=mean(temp_anom)) |> as.numeric() -> mtddavg
-#Sep to date: 0.710
+#Nov to date: 0.984
 
 
 ### force Aug
 
 t <- get_temp()
 
-t |> rowwise() |> mutate(ytd=mean(c(Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep))) |> ungroup() -> t
+t |> rowwise() |> mutate(ytd=mean(c(Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct))) |> ungroup() -> t
 
 t |> mutate(diff = `J-D`- ytd) -> t
 
 tail(head(t, -1), 7) |> summarize(diff=mean(diff)) |> as.numeric() -> temp_diff_new
 
 # August
-mean(c(as.numeric(tail(t,1)[2:8]), 0.77049 + 0.4999)) + -0.007857 ##1.056
+mean(c(as.numeric(tail(t,1)[2:8]), 0.77049 + 0.4999)) + -0.007857 ##1.051
+
 
 # September
-mean(c(as.numeric(tail(t,1)[2:9]), mtddavg + daily_diff)) + temp_diff_new ##1.084 Sep 14
+mean(c(as.numeric(tail(t,1)[2:9]), 0.9803 + 0.4511)) + -0.0050 ##1.086
 
-# ALL --> 1.103
-mean(c(as.numeric(tail(t,1)[2:9]), mtddavg + daily_diff, 0.69 + daily_diff,0.69 + daily_diff,0.69 + daily_diff))
+# October
+mean(c(as.numeric(tail(t,1)[2:10]), 0.9211 + 0.4261)) + -0.0050 ##1.116
+
+
+# November
+mean(c(as.numeric(tail(t,1)[2:11]), mtddavg + daily_diff)) + temp_diff_new
+
+# ALL --> 1.135
+mean(c(as.numeric(tail(t,1)[2:11]), mtddavg + daily_diff, 0.69 + daily_diff))
 
 # plot
 library(ggplot2)
-d |> filter(year==2023 | year==2022) |> filter(dummy_date > as.Date("1975-09-01")) |>
-  ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=1) + geom_smooth() + theme_minimal()
+d |> filter(year==2022 | year==2023 | year==2021) |> filter(dummy_date > as.Date("1975-09-01")) |>
+  ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=1) + geom_smooth() + theme_minimal() +labs(color='Year')
 
 plot_dailytemp()
 
