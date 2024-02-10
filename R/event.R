0ca953482d9c5b3dc205aed422ee4dcc3d965593
Hernando Cortina <hch@alum.mit.edu>
2024-02-10 12:06:35 -0500

    Feb heat

--- R/event.R
+++ R/event.R
@@ -104,50 +104,49 @@ ggplot(l,aes(x=Name,y=Amount,fill=col)) +geom_col() + theme_minimal() + guides(f
 
 ##########################
 
-
-# plot Jan kalshi
-library(ggplot2)
+# plot Feb kalshi
 library(dplyr)
-
+library(ggplot2)
+library(hockeystick)
 
 d <- get_dailytemp(use_cache = F, write_cache = T)
 
-d |> filter(year==2020 | year==2023 | year==2024) |> filter(dummy_date > as.Date("1925-01-01")) |>
+d |> filter(year==2016 | year==2023 | year==2024) |> filter(dummy_date > as.Date("1925-01-01")) |>
   ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=0) + geom_smooth(se = F) + scale_y_continuous(n.breaks=6) +
   theme_bw(base_size = 13) +labs(title='World Daily Average Air Temperature', subtitle='2-meter air temperature', x='Date',color ='Year',y='Temp Anomaly (C)', caption = paste0("Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ", pull(tail(d,1)["date"]))) +
   scale_x_date(date_labels="%b") + scale_color_manual(values = c("darkgreen", "red", "dodgerblue"))
 
 
-d |> filter(dummy_date <= as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd)
-d |> filter(dummy_date <= as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd) |> pull(ytd, name=year) |> rev() |> diff()
+d |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd)
+d |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd) |> pull(ytd, name=year) |> rev() |> diff()
 
 
-d |> filter(year==2020 | year==2024) |> filter(dummy_date <= as.Date("1925-01-31")) |> select(year, dummy_date, temp_anom) |> tidyr::pivot_wider(names_from = year, values_from = temp_anom)
+d |> filter(year==2016 | year==2024) |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> select(year, dummy_date, temp_anom) |> tidyr::pivot_wider(names_from = year, values_from = temp_anom)
 
 
-d |> filter(year==2020 | year==2023 | year==2024) |> filter(dummy_date > as.Date("1925-01-01") & dummy_date < as.Date("1925-02-01")) |>
-     ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=0) + geom_line(linewidth=1) + scale_y_continuous(n.breaks=12) +
-     theme_bw(base_size = 13) +labs(title='World Daily Average Air Temperature', subtitle='2-meter air temperature', x='Date',color ='Year',y='Anomaly (C)', caption = paste0("Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ", pull(tail(d,1)["date"]))) +
-     scale_x_date(date_labels="%m/%d") + scale_color_manual(values = c("darkgreen", "red", "dodgerblue"))
+d |> filter(year==2016 | year==2023 | year==2024) |> filter(dummy_date >= as.Date("1925-02-01") & dummy_date < as.Date("1925-03-01")) |>
+  ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=0) + geom_line(linewidth=1) + scale_y_continuous(n.breaks=12) +
+  theme_bw(base_size = 13) +labs(title='World Daily Average Air Temperature', subtitle='2-meter air temperature', x='Date',color ='Year',y='Anomaly (C)', caption = paste0("Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ", pull(tail(d,1)["date"]))) +
+  scale_x_date(date_labels="%m/%d") + scale_color_manual(values = c("darkgreen", "red", "dodgerblue"))
 
 # FORECAST REST OF MONTH
 extra <- tail(d,1)
 
-# 0.10 is breakeven as of Jan 8
-fcst <- pull(extra[1,6]) - 0.10
+# 0.28 is breakeven as of Feb 2
+fcst <- pull(extra[1,6]) - 0.28
 
-extra <- data.frame(year=rep(2024, 31-pull(extra[1,2])), day_of_year=(pull(extra[1,2])+1):31, date=NA,temp=NA,`1979-2000 mean`=NA, temp_anom=fcst,dummy_date=pull(extra[1,7])+1:(31-pull(extra[1,2])))
+extra <- data.frame(year=rep(2024, 60-pull(extra[1,2])), day_of_year=(pull(extra[1,2])+1):60, date=NA,temp=NA,`1979-2000 mean`=NA, temp_anom=fcst,dummy_date=pull(extra[1,7])+1:(60-pull(extra[1,2])))
 f <- bind_rows(d,extra)
 
 
-f |> filter(dummy_date <= as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=round(mean(temp_anom),digits = 2)) |> slice_max(n=10, order_by=ytd)
-f |> filter(dummy_date <= as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd) |> pull(ytd, name=year) |> rev() |> diff()
+f |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=round(mean(temp_anom),digits = 2)) |> slice_max(n=10, order_by=ytd)
+f |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> group_by(year) |> summarize(ytd=mean(temp_anom)) |> slice_max(n=10, order_by=ytd) |> pull(ytd, name=year) |> rev() |> diff()
 
 
-f |> filter(year==2020 | year==2024) |> filter(dummy_date > as.Date("1925-01-01") & dummy_date < as.Date("1925-02-01")) |>
+f |> filter(year==2016 | year==2024) |> filter(dummy_date > as.Date("1925-01-01") & dummy_date < as.Date("1925-03-01")) |>
   ggplot(aes(x=dummy_date, y=temp_anom, color=as.factor(year))) + geom_point(size=0) + geom_line(linewidth=1) + scale_y_continuous(n.breaks=12) +
   theme_bw(base_size = 13) +labs(title='World Daily Average Air Temperature', subtitle='2-meter air temperature', x='Date',color ='Year',y='Anomaly (C)', caption = paste0("Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ", pull(tail(d,1)["date"]))) +
   scale_x_date(date_labels="%m/%d") + scale_color_manual(values = c("darkgreen", "red", "dodgerblue"))
 
 
-f |> filter(year==2020 | year==2024) |> filter(dummy_date <= as.Date("1925-01-31")) |> select(year, dummy_date, temp_anom) |> tidyr::pivot_wider(names_from = year, values_from = temp_anom)
+f |> filter(year==2016 | year==2024) |> filter(dummy_date < as.Date("1925-03-01"), dummy_date > as.Date("1925-01-31")) |> select(year, dummy_date, temp_anom) |> tidyr::pivot_wider(names_from = year, values_from = temp_anom)
