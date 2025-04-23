3ac3723798d62a5bdeda785d23052b7ee0013e61
Hernando Cortina <hch@alum.mit.edu>
2025-04-23 16:34:43 -0400

    minor update

--- R/event.R
+++ R/event.R
@@ -441,10 +441,10 @@ end <- ceiling_date(as.Date(start), "month")-1
 
 fcstcop <- cop |> filter(date>=as.Date(start), date <=as.Date(end)) |> select(temp_anom) |> summarize(mean(temp_anom)) |> pull()
 fcstloti <- round(fcstcop + gap, 3)
-# April19: 1.214
+# April20: 1.212
 f |> filter(dummy_date <= as.Date("1925-04-30"), dummy_date >= as.Date("1925-04-01")) |> group_by(year) |> summarize(mtd=round(mean(temp_anom),digits = 3)) |> slice_max(n=5, order_by=mtd) |> filter(year==2025) |> pull(mtd) -> fcstcop
 fcstloti <- round(fcstcop + gap, 3)
-# April19: 1.2
+# April20: 1.192
 
 #### FRED ####
 
--- R/spx.R
+++ R/spx.R
@@ -13,16 +13,16 @@ calc <- SPX
 names(calc)<-c('SPX', 'ma50', 'ma200')
 plot(calc['2024::'], legend.loc = 'top', grid.ticks.lty = 3)
 fwd1 <- lag.xts(SPX, k=-21*1)
+fwd2 <- lag.xts(SPX, k=-21*2)
 fwd3 <- lag.xts(SPX, k=-21*3)
 fwd6 <- lag.xts(SPX, k=-21*6)
 fwd9 <- lag.xts(SPX, k=-21*9)
-fwd12 <- lag.xts(SPX, k=-21*12)
-calc <- merge.xts(calc, fwd1$SPX.Close,fwd3$SPX.Close, fwd6$SPX.Close, fwd9$SPX.Close, fwd12$SPX.Close)
-names(calc)<-c('SPX', 'ma50', 'ma200', 'fwd1', 'fwd3', 'fwd6', 'fwd9', 'fwd12')
+calc <- merge.xts(calc, fwd1$SPX.Close,fwd2$SPX.Close, fwd3$SPX.Close, fwd6$SPX.Close, fwd9$SPX.Close)
+names(calc)<-c('SPX', 'ma50', 'ma200', 'fwd1', 'fwd2', 'fwd3', 'fwd6', 'fwd9')
 rm(list=ls(pattern = 'fwd'))
 
 calc <- data.frame(date=index(calc), spx=calc$SPX, ma50=calc$ma50, ma200=calc$ma200, mo1=calc$fwd1,
-                   mo3=calc$fwd3, mo6=calc$fwd6, mo9=calc$fwd9, mo12=calc$fwd12)
+                   mo2=calc$fwd2, mo3=calc$fwd3, mo6=calc$fwd6, mo9=calc$fwd9)
 rownames(calc) <- NULL
 calc <- calc |> mutate(ind=sign(ma50-ma200))
 calc <- calc |> mutate(swap=ind==lag(ind))
@@ -30,10 +30,13 @@ calc <- calc |> mutate(dc=(!swap & (ind==-1)))
 calc |> filter(dc==T) -> dc
 
 dc |> select(date, SPX, starts_with('fwd')) -> dc
-dc |> mutate(ret1=fwd1/SPX-1, ret3=fwd3/SPX-1, ret6=fwd6/SPX-1, ret9=fwd9/SPX-1,ret12=fwd12/SPX-1) -> dc
+dc |> mutate(ret1=fwd1/SPX-1, ret2=fwd2/SPX-1, ret3=fwd3/SPX-1, ret6=fwd6/SPX-1,ret9=fwd9/SPX-1) -> dc
 dc |> drop_na() -> dc
 
-dc |> select(starts_with('ret')) |> colMeans()
-dc |> select(starts_with('ret')) |> sign() |> colSums()+nrow(dc) -> pos
-(pos *.5)/nrow(dc)
-hist(dc$ret3)
+dc |> summarize(across(starts_with('ret'), mean))
+dc |> summarize(across(starts_with('ret'), fivenum))
+dc |> select(starts_with('ret')) |> sign() |> summarize_all(\(x) (x>0)) |> summarize_all(sum)/nrow(dc)
+
+ggplot(dc,aes(x=ret1)) + geom_bar(fill='darkgrey') + scale_y_continuous(breaks = seq(0,10,2)) +
+  scale_x_binned(n.breaks = 6) +theme_bw() +labs(x='return')
+
