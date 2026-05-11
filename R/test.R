b67e3246722060a5cc9e8a27800f14790dbfc86c
Hernando Cortina <hch@alum.mit.edu>
2026-05-11 16:21:31 -0400

    update getsymbols

--- R/test.R
+++ R/test.R
@@ -1,18 +1,43 @@
 library(quantmod)
 library(PerformanceAnalytics)
 
-getSymbols('^SPX', from = '2017-01-01')
-getSymbols('SPY', from = '2017-01-01')
-getSymbols('RSP', from = '2017-01-01')
+getSymbols('BRKU', from = '2024-12-11')
+getSymbols('BRKW', from = '2024-12-11')
+getSymbols('BRK-B', from = '2022-12-11')
 
-sp <- merge(SPX$SPX.Adjusted, SPY$SPY.Adjusted, RSP$RSP.Adjusted)
+chartSeries(`BRK-B`, type='line',TA=NULL,theme = chartTheme('white',up.col='orange',dn.col='red'))
+
+sp <- merge(BRKU$BRKU.Adjusted, BRKW$BRKW.Adjusted, `BRK-B`$'BRK-B.Adjusted')
 
 spret <- Return.calculate(sp)
-spret <- spret['2021/']
+spret <- spret['2024/']
 Return.annualized(spret)
 
-TrackingError(spret$SPX.Adjusted, spret$SPY.Adjusted)
+TrackingError(spret$BRKU.Adjusted, spret$BRK.B.Adjusted)
 
-chart.RelativePerformance(spret$SPX.Adjusted, spret$SPY.Adjusted)
+chart.RelativePerformance(spret$BRKU.Adjusted, spret$BRKW.Adjusted)
 
 
+
+######## Normalized plots
+normalize_to_100 <- function(x) {
+  if (length(x) == 0 || is.na(x[1])) return(x) # handle empty or NA
+  return((x / as.numeric(x[1])) * 100)
+}
+
+ts1_norm <- normalize_to_100(BRKU$BRKU.Adjusted)
+ts2_norm <- normalize_to_100(`BRK-B`$'BRK-B.Adjusted')
+ts3_norm <- normalize_to_100(`BRKW`$'BRKW.Adjusted')
+
+# Combine into one xts object
+combined <- merge(ts1_norm, ts2_norm, ts3_norm)
+colnames(combined) <- c("BRKU", "BRK", "BRKW")
+
+# Plot both series in one chart
+plot(combined,
+     multi.panel = FALSE,
+     col = c("blue", "red","orange"),
+     lwd = 2,
+     main = "Multiple xts Series Normalized to 100",
+     ylab = "Index (Start = 100)",
+     legend.loc = "top")
