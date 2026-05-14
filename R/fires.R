5e844b496e2dfb32062211e5b04e343227161715
Hernando Cortina <hch@alum.mit.edu>
2026-05-14 14:51:59 -0400

    add ggplot

--- R/fires.R
+++ R/fires.R
@@ -98,3 +98,40 @@ get_fires_emissions <- function(place='WORLD', year=as.numeric(format(Sys.Date()
 
   if (write_cache) saveRDS(fires, file.path(hs_path, cachename))
   return(emissions) }
+
+
+
+
+
+#=======
+ggplot(df, aes(x = date)) +theme_minimal() +geom_ribbon(data=df,aes(ymin=cum_area_ha_min, ymax=cum_area_ha_max), fill='gray90') + geom_line(aes(y=cum_area_ha),col='red') + geom_line(data=df, aes(y=cum_area_ha_avg),col="dodgerblue") +
+  labs(y='Cumulative Burnt Area (thousanfs of ha)', x=NULL, title='Wildfire Burnt Area') + scale_y_continuous(labels = scales::label_comma(scale = .001))
+
+
+
+ggplot(df, aes(x = date)) +theme_minimal() +geom_ribbon(data=df,aes(ymin=area_ha_min, ymax=area_ha_max),fill='gray90') + geom_line(aes(y=area_ha),col='red') + geom_line(data=df, aes(y=area_ha_avg),col="blue")
+
+
+
+
+ggplot(df, aes(x = date)) +theme_bw(base_size = 13) +geom_ribbon(aes(ymin=cum_area_ha_min, ymax=cum_area_ha_max, fill='Min - Max Range\n(since 2012)')) +
+  geom_line(aes(y=cum_area_ha, col='Year-to-date'), linewidth=1.1, na.rm = T) + geom_line(aes(y=cum_area_ha_avg, col="Average\n(since 2012)")) +
+  labs(y='Cumulative Burnt Area (thousands of ha)', x=NULL, title='2026 Wildfire Area Burnt', subtitle='World',caption='Source: GWIS') + scale_y_continuous(n.breaks = 8, labels = scales::label_comma(scale = .001)) +
+  scale_colour_manual("",values=c("red", "dodgerblue"), breaks=c("Year-to-date","Average\n(since 2012)"))+
+  scale_fill_manual('', values="grey90") +theme(legend.position = "top")
+
+# num fires
+ggplot(df, aes(x = date)) +theme_bw(base_size = 13) +geom_ribbon(aes(ymin=cum_events_min, ymax=cum_events_max, fill='Min - Max Range\n(since 2012)')) +
+  geom_line(aes(y=cum_events, col='Year-to-date'), linewidth=1.1) + geom_line(aes(y=cum_events_avg, col="Average\n(since 2012)")) +
+  labs(y='Cumulative Number', x=NULL, title='2026 Number of Wildfires', subtitle='World',caption='Source: GWIS') + scale_y_continuous(n.breaks = 8, labels = scales::label_comma()) +
+  scale_colour_manual("",values=c("red", "dodgerblue"), breaks=c("Year-to-date","Average\n(since 2012)"))+
+  scale_fill_manual('', values="grey90") +theme(legend.position = "top")
+
+
+####### emissions
+
+e |> filter(plt=='CO2') |> ggplot(aes(x = date)) +theme_bw(base_size = 13) +geom_ribbon(aes(ymin=cum_minv, ymax=cum_maxv, fill='Min - Max Range\n(since 2003)')) +
+  geom_line(aes(y=cum_curvs, col='Year-to-date'), linewidth=1.1) + geom_line(aes(y=cum_avvg, col="Average\n(since 2003)")) + labs(y='Cumulative Emissions (millions of tons)', x=NULL, title='2026 Emissions from Wildfires', subtitle='World',caption='Source: GWIS') + scale_y_continuous(n.breaks = 8, labels = scales::label_comma(scale = .000001)) +
+  scale_colour_manual("",values=c("red", "dodgerblue"), breaks=c("Year-to-date","Average\n(since 2003)"))+
+  scale_fill_manual('', values="grey90") +theme(legend.position = "top")
+
